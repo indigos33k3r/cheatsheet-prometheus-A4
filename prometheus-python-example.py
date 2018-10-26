122cc80b784e99dd157cc60f4fe0fadad58b7002
Denny Zhang <denny.zhang001@gmail.com>
2018-10-25 22:46:57 -0700

    prometheus python exporter

--- prometheus-python-example.py
+++ prometheus-python-example.py
@@ -15,6 +15,7 @@ def process_request(t):
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
+    # http://localhost:8000/metrics
     start_http_server(8000)
     # Generate some requests.
     while True:
